f56f1253dba065b595e8be99aefeedad4e4e53d9
Sanya Vashist <110304904+Shado-ow@users.noreply.github.com>
2024-02-21 12:56:11 +0530

    Update matrix_multiplication_2x2.s

--- lab3/matrix_multiplication_2x2.s
+++ lab3/matrix_multiplication_2x2.s
@@ -17,30 +17,5 @@ LDR R4,=m1
 LDR R5,=m2
 LDR R6,=res
 
-outer:
-SUB R0,R0,#1
-MOV R1,#3
-inner:
-    LDR R2,[R4],#4
-    LDR R3,[R5],#4
-    ADD R7,R2,R3
 
-    STR R7,[R6],#4
-
-    ;update arrays
-    ;ADD R4,R4,#4
-    ;ADD R5,R5,#4
-    ;ADD R6,R6,#4
-
-    ;loop
-    SUB R1,R1,#1
-    CMP R1,#0
-    BNE inner
-
-CMP R0,#0
-BNE outer
-  STR r6,[r3],#4
-  SUB r8,r8,#1
-  CMP r8,#0
-  BNE loop
 SWI 0x11
